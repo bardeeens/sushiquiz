4166133a7ac050fd8b13922b61791ba985e748d4
Bardini <derekbardini@gmail.com>
2021-01-07 23:39:50 -0600

    1st question with image now displays

--- Assets/script.js
+++ Assets/script.js
@@ -1,34 +1,107 @@
 var timerEl = document.getElementById("timer")
-var timeRemaining = 6
+var timeTilTest = 6
+var testDuration = 91
 var startButton = document.getElementById("start-button")
-var questions = [
+var questionIndex = 0;
+var entireQuiz = [
     {
-        q: "What fish is this?",
-        choices: [1, 2, 3],
-        answer: "maguro"
+        q: "Assets/Images/salmon.jpg",
+        choices: ["salmon", "octopus", "tuna", "crab"],
+        answer: "salmon"
+    },
+    {
+        q: "/Assets/Images/salmonwcilantro.jpg",
+        choices: ["salmon", "octopus", "tuna", "crab"],
+        answer: "salmon"
     }
 ];
 
 function countDownToQuiz() {
     var timerInterval = setInterval(function() {
-        timeRemaining--;
-        timerEl.textContent = "Quiz will begin in " + timeRemaining + " seconds!";
+        timeTilTest--;
+        timerEl.textContent = "Quiz will begin in " + timeTilTest + " seconds!";
 
-        if(timeRemaining === 1) {
-            timerEl.textContent = "Quiz will begin in " + timeRemaining + " second!";
-        } else if (timeRemaining === 0) {
+        if(timeTilTest === 1) {
+            timerEl.textContent = "Quiz will begin in " + timeTilTest + " second!";
+        } else if (timeTilTest === 0) {
             clearInterval(timerInterval)
             timerEl.textContent = "Go!!!"
             beginQuiz();
+            testTimer();
+            startButton.setAttribute("class", "hidden");
         }
     }, 1000)
 }
 
+function testTimer() {
+    var timerInterval = setInterval(function() {
+        testDuration--;
+        timerEl.textContent = testDuration + " seconds left";
+
+        if(testDuration === 1) {
+            timerEl.textContent = testDuration + " second left";
+        } else if (testDuration === 0) {
+            clearInterval(timerInterval)
+            timerEl.textContent = "You have ran out of time!"
+            // beginQuiz();
+        }
+    }, 1000)
+    
+}
+
+function displayQuestion() {
+// 
+var questionImage = document.createElement("img");
+    questionImage.setAttribute("src", entireQuiz[questionIndex].q)
+    document.querySelector(".box").appendChild(questionImage)
+
+
+    for (let i = 0; i < entireQuiz[questionIndex].choices.length; i++) {
+
+        const choice = entireQuiz[questionIndex].choices[i]
+        var button = document.createElement("button");
+        button.textContent = choice;
+        button.addEventListener("click", function(){
+            var userChoice = this.textContent
+            console.log(userChoice);
+            checkAnswer(userChoice);
+            // get text value from choice
+            // check answer 
+            //if answer = choice, right
+            // if right score increase
+            //if answer != choice, wrong
+            //if wrong timer -5
+            //
+        })
+        document.querySelector(".box").appendChild(button);
+
+    }
+
+}
+
+
+function currentAnswer() {
+    return entireQuiz[questionIndex].answer
+}
+
+function checkAnswer(userChoice) {
+   if (userChoice === currentAnswer()) {
+       
+   }
+   
+   
+   
+   
+    questionIndex++;
+}
+
 function beginQuiz() {
     var question = document.createElement("main");
-
-    question.textContent = "What fish is this?"
+    question.textContent = "What kind of sushi is this?";
+    question.setAttribute("class", "question");
     document.querySelector(".box").appendChild(question);
+
+    displayQuestion();
 };
 
 startButton.addEventListener("click", function(){
@@ -36,3 +109,4 @@ startButton.addEventListener("click", function(){
 
     });
 
+console.log(entireQuiz[0].q);
